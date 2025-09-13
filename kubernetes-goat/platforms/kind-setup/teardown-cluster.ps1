cf9d338b6bc241fd5a679d9ab824ffc72d45985d
Raushan  Raj <raushanraj@192.168.1.7>
2025-09-13 14:18:23 +0530

    many changes

--- kubernetes-goat/platforms/kind-setup/teardown-cluster.ps1
+++
@@ -1,5 +0,0 @@
-# This file has been created to contribute to the development of the Kubernetes Goat
-
-# Destroy cluster and delete everyting
-../../teardown-kubernetes-goat.ps1
-kind delete cluster --name kubernetes-goat-cluster
