6c47ffa620823c0f411780d21099bd7e0de1b2ee
Raushan  Raj <raushanraj@192.168.1.7>
2025-09-13 11:37:35 +0530

    checking

--- Damn-vulnerable-sca/xz-java-malicious/src/main/java/org/tukaani/xz/IndexIndicatorException.java
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: 0BSD
-// SPDX-FileCopyrightText: The XZ for Java authors and contributors
-// SPDX-FileContributor: Lasse Collin <lasse.collin@tukaani.org>
-
-package org.tukaani.xz;
-
-class IndexIndicatorException extends Exception {
-    private static final long serialVersionUID = 1L;
-}
